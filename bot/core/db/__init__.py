27a736db11b8814d611403a05d8b00749d8a567a
kirill-jjj <kirillkolovi@gmail.com>
2025-07-04 15:28:53 +0500

    Revert "Implement admin approval for web registrations"
    This reverts commit f7ca93c39c82d0bc4682828965628d05fb736d0a.

--- bot/core/db/__init__.py
+++ bot/core/db/__init__.py
@@ -17,19 +17,12 @@ from .crud import (
     is_telegram_id_registered,
     mark_fastapi_download_token_used,
     remove_fastapi_download_token,
-    # PendingWebRegistration CRUD functions
-    add_pending_web_registration,
-    get_pending_web_registration_by_id,
-    get_all_pending_web_registrations,
-    remove_pending_web_registration,
-    cleanup_expired_pending_web_registrations,
 )
 from .models import (
     Base,
     FastapiDownloadToken,
     FastapiRegisteredIp,
     PendingTelegramRegistration,
-    PendingWebRegistration, # Added PendingWebRegistration model
     TelegramRegistration,
 )
 
@@ -41,10 +34,9 @@ from .session import AsyncSessionLocal, async_engine, close_db_engine, init_db
 __all__ = [
     "Base",
     "TelegramRegistration",
-    "PendingTelegramRegistration",
-    "PendingWebRegistration", # Added
-    "FastapiRegisteredIp",
-    "FastapiDownloadToken",
+    "PendingTelegramRegistration", # Added
+    "FastapiRegisteredIp",       # Added
+    "FastapiDownloadToken",      # Added
     "async_engine",
     "AsyncSessionLocal",
     "init_db",
@@ -52,21 +44,15 @@ __all__ = [
     "is_telegram_id_registered",
     "add_telegram_registration",
     "get_teamtalk_username_by_telegram_id",
-    "add_pending_telegram_registration",
-    "get_and_remove_pending_telegram_registration",
-    "cleanup_expired_pending_registrations",
-    "add_fastapi_registered_ip",
-    "is_fastapi_ip_registered",
-    "cleanup_expired_registered_ips",
-    "add_fastapi_download_token",
-    "get_fastapi_download_token",
-    "mark_fastapi_download_token_used",
-    "remove_fastapi_download_token",
-    "cleanup_expired_download_tokens",
-    # Added PendingWebRegistration CRUD functions to __all__
-    "add_pending_web_registration",
-    "get_pending_web_registration_by_id",
-    "get_all_pending_web_registrations",
-    "remove_pending_web_registration",
-    "cleanup_expired_pending_web_registrations",
+    "add_pending_telegram_registration",      # Added
+    "get_and_remove_pending_telegram_registration", # Added
+    "cleanup_expired_pending_registrations",  # Added
+    "add_fastapi_registered_ip",              # Added
+    "is_fastapi_ip_registered",               # Added
+    "cleanup_expired_registered_ips",         # Added
+    "add_fastapi_download_token",             # Added
+    "get_fastapi_download_token",             # Added
+    "mark_fastapi_download_token_used",       # Added
+    "remove_fastapi_download_token",          # Added
+    "cleanup_expired_download_tokens"         # Added
 ]
